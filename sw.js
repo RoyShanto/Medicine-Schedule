c3b58458544f856e1826810ebcb3c3cb7f7b0675
Shanto Roy <roycse17@gmail.com>
2026-02-23 23:27:16 +0600

    v-1

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "medicine-manager-v1";
+
+const urlsToCache = [
+    "/Medicine-Schedule/",
+    "/Medicine-Schedule/index.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
