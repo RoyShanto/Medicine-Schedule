9ce78c99a4bde7605b5c17044c27c580a519bd09
Shanto Roy <roycse17@gmail.com>
2026-03-07 10:48:58 +0600

    added refresh and change design

---
+++ medicines.js
@@ -0,0 +1,15 @@
+export const medicines = [
+    { name: "Siglimet TM 50/500", morning: 1, afterFood: true },
+    { name: "Rosu 5", night: 1, afterFood: true },
+    { name: "Salazine 500", morning: 2, night: 2, afterFood: true, note: "First 5 days [18-Feb-22-Feb]: 0.5 + 0.5, Next 5 days [23-Feb-27-Feb]: 1 + 1, Next 5 days [28-Feb - 04-Mar]: 2 + 1, Next 5 days [05-Mar - 09-Mar]: 2 + 2" },
+    { name: "Fexo 120", morning: 1, afterFood: true },
+    { name: "Montene 10", night: 1, afterFood: true },
+    { name: "Xyril 25", night: 1, afterFood: true },
+    { name: "Opal 20 (Gastric)", night: 1, beforeFood: true }
+];
+
+
+
+export const others = [
+    { name: "Lorix Plus (Lotion)", morning: 1, noon: 1, night: 1, note: "Use after shower, Dry body properly, Use 3 days per week." }
+];
